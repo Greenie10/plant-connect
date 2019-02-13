596ec72d283798139f3afbbb2a5a86294944fe9e
lauragreen <lollymay@gmail.com>
2019-02-13 12:55:01 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# graphql-user-api
-This is a graphql based API that resolves using a REST API. The purpose of this repo is to show how devs and organizations can start using graphql with the least effort possible and no re-writing their existing infrastructure.
+# plant-connect
+Wraps the RESTful [Trefle API](https://trefle.io/) in GraphQL.
+
+## graphql-user-api
+This repo is based upon [`graphql-user-api`](https://github.com/joeynimu/graphql-user-api): a graphql based API that resolves using a REST API. The purpose of `graphql-user-api` is to show how devs and organizations can start using graphql with the least effort possible and no re-writing their existing infrastructure.
+_from:_ [Wrapping a REST API in GraphQL](https://medium.com/@joeynimu/wrapping-a-rest-api-in-graphql-f50c7b9669d5) by [Joey Ng'ethe](https://medium.com/@joeynimu)
--- src/server.js
+++ src/server.js
@@ -1,27 +1,26 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import fetch from 'node-fetch';
 import bodyParser from 'body-parser';
 
-import { APP_PORT } from './constants'
-import schema from './schema'
+import { APP_PORT } from './constants';
+import schema from './schema';
 
-const app  = express();
+const app = express();
 
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(graphqlHTTP({
-    graphiql: true,
-    schema,
-    pretty: true
+  graphiql: true,
+  schema,
+  pretty: true,
 }));
 
 try {
-  app.listen(APP_PORT, () => console.log(`GraphQL server running at ${APP_PORT}`))
+  app.listen(APP_PORT, () => console.log(`GraphQL server running at ${APP_PORT}`));
 } catch (error) {
-  console.log(`Something went wrong ${error}`)
+  console.log(`Something went wrong ${error}`);
 }
